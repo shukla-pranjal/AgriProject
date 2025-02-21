3b5ec34795320ec7709b98548019648af100c567
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-02-21 22:39:47 +0530

    done

---
+++ AgriProject_java/src/main/java/com/agriproject/AgriProjectApplication.java
@@ -0,0 +1,13 @@
+package com.agriproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgriProjectApplication.class, args);
+    }
+
+}
---
+++ AgriProject_java/src/test/java/com/agriproject/AgriProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agriproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
