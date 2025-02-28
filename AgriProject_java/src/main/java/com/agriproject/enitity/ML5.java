62c6d49e82d8608f4a98c4bae87ebea0de74ac55
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-02-28 21:56:32 +0530

    changed data settings in application.properties

---
+++ AgriProject_java/src/main/java/com/agriproject/enitity/ML5.java
@@ -0,0 +1,14 @@
+package com.agriproject.enitity;
+
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class ML5 {
+}
