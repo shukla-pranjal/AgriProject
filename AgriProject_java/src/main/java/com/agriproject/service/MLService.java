2d8abc9889d5bfd9a2c827ad8403c2450c205a05
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-03-02 23:05:12 +0530

    Introduced MLService and its implementation for predictive analysis integration

--- AgriProject_java/src/main/java/com/agriproject/service/MLService.java
+++ AgriProject_java/src/main/java/com/agriproject/service/MLService.java
@@ -1,4 +1,11 @@
 package com.agriproject.service;
 
-public class MLService {
+import com.agriproject.dto.ML5Dto;
+import org.springframework.stereotype.Service;
+
+//@Service
+public interface MLService {
+
+
+    String predictM5(ML5Dto ml5Dto);
 }
