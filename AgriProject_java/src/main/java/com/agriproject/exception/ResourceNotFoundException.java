73fbe305cb02b859c4625789c1527e002630e85e
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-03-16 11:26:48 +0530

    ❌ Introduced ResourceNotFoundException for clearer error signaling on entity fetch failures

---
+++ AgriProject_java/src/main/java/com/agriproject/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.agriproject.exception;
+
+
+public class ResourceNotFoundException extends Exception{
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+    
+}
