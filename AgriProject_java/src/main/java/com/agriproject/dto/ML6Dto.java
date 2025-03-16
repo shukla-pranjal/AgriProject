f164ca01c26b5026a900a18a9d493b184d737ab1
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-03-16 11:21:21 +0530

    feat(dto): add ML6Dto for ML model integration (v6)

--- AgriProject_java/src/main/java/com/agriproject/dto/ML6Dto.java
+++ AgriProject_java/src/main/java/com/agriproject/dto/ML6Dto.java
@@ -1,4 +1,15 @@
 package com.agriproject.dto;
 
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+@NoArgsConstructor
 public class ML6Dto {
+    private Integer id;
+    private Integer moisture;
+    private Integer temp;
 }
