fc2f48b44ebb30039c1c41b8e00b01df09912855
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-03-20 11:26:48 +0530

    🔗 Improved FlaskService communication & strengthened ValidationException handling

--- AgriProject_java/src/main/java/com/agriproject/exception/ValidationException.java
+++ AgriProject_java/src/main/java/com/agriproject/exception/ValidationException.java
@@ -1,4 +1,16 @@
 package com.agriproject.exception;
 
-public class ValidationException {
+import java.util.Map;
+
+public class ValidationException extends RuntimeException {
+    private Map<String, Object> error;
+
+
+    public ValidationException(Map<String, Object> error){
+        super("Validation Failed");
+        this.error = error;
+    }
+    public Map<String, Object> getError() {
+        return error;
+    }
 }
--- AgriProject_java/src/main/java/com/agriproject/external/FlaskService.java
+++ AgriProject_java/src/main/java/com/agriproject/external/FlaskService.java
@@ -1,4 +1,29 @@
 package com.agriproject.external;
 
-public class FlaskService {
+
+import com.agriproject.dto.ML5Dto;
+import com.agriproject.dto.ML6Dto;
+import com.agriproject.dto.ML7Dto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Map;
+
+@FeignClient(name= "ML-SERVICE")
+public interface FlaskService {
+
+
+    @PostMapping("/predict/5")
+    Map<String, Object> model5(@RequestBody ML5Dto ml5Dto);
+
+    @PostMapping("/predict/6")
+    Map<String, Object> model6(@RequestBody ML6Dto ml6Dto);
+
+    @PostMapping("/predict/7")
+    Map<String, Object> model7(@RequestBody ML7Dto ml7Dto);
+
+
+
 }
