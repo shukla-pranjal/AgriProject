12aaa0f9767b9c32886f9c94a0803f12c0c6240a
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-03-23 11:21:21 +0530

    feat(dto): added ML7Dto for version 7 ML model integration

--- AgriProject_java/src/main/java/com/agriproject/dto/ML7Dto.java
+++ AgriProject_java/src/main/java/com/agriproject/dto/ML7Dto.java
@@ -1,4 +1,25 @@
 package com.agriproject.dto;
 
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+@NoArgsConstructor
 public class ML7Dto {
+    private Integer id;
+    private int temparature;
+    private int humidity;
+    private int moisture;
+    private int nitrogen;
+    private int potassium;
+    private int phosphorous;
+    @JsonProperty("soil_type")
+    private String soilType;
+    @JsonProperty("crop_type")
+    private String cropType;
+
 }
