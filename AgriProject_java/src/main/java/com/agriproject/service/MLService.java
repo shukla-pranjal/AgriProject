41cf473086bbd0c35d0db53d6de5b3d70995079c
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-03-28 11:26:48 +0530

    📊 Refined ML service abstraction and backend implementation logic

--- AgriProject_java/src/main/java/com/agriproject/service/MLService.java
+++ AgriProject_java/src/main/java/com/agriproject/service/MLService.java
@@ -1,11 +1,15 @@
 package com.agriproject.service;
 
 import com.agriproject.dto.ML5Dto;
-import org.springframework.stereotype.Service;
+import com.agriproject.dto.ML6Dto;
+import com.agriproject.dto.ML7Dto;
 
-//@Service
+
 public interface MLService {
 
-
     String predictM5(ML5Dto ml5Dto);
+
+    String predictM6(ML6Dto ml6Dto);
+
+    String predictM7(ML7Dto ml7Dto);
 }
--- AgriProject_java/src/main/java/com/agriproject/service/impl/MLServiceImpl.java
+++ AgriProject_java/src/main/java/com/agriproject/service/impl/MLServiceImpl.java
@@ -1,34 +1,59 @@
 package com.agriproject.service.impl;
 
-import com.agriproject.dto.ML5Dto;
-import com.agriproject.service.MLService;
-import com.agriproject.util.Constants;
-import org.springframework.http.ResponseEntity;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestOperations;
-import org.springframework.web.client.RestTemplate;
 
-import static com.agriproject.util.Constants.FLASK_API_URL;
+import com.agriproject.dto.ML5Dto;
+import com.agriproject.dto.ML6Dto;
+import com.agriproject.dto.ML7Dto;
+import com.agriproject.exception.FlaskServiceException;
+import com.agriproject.external.FlaskService;
+import com.agriproject.service.MLService;
+import com.agriproject.util.Validation;
 
+import feign.FeignException;
 
 @Service
-public class MLServiceImpl implements MLService {
+public class MLServiceImpl  implements MLService {
 
+
+    @Autowired
+    private FlaskService flaskService;
+
+    @Autowired
+    private Validation validation;
+
+
+    private <T> String getPrediction(Function<T, Map<String, Object>> flaskMethod, T requestDto) {
+        try {
+            Map<String, Object> response = flaskMethod.apply(requestDto);
+            return (String) response.get("response"); // Extract "response"
+        } catch (FeignException e) {
+            throw e;
+        }
+        catch (Exception e) {
+            throw new FlaskServiceException(e.getMessage());
+        }
+    }
 
     @Override
     public String predictM5(ML5Dto ml5Dto) {
-        RestTemplate restTemplate = new RestTemplate();
+        validation.model5Validation(ml5Dto);
+        return getPrediction(flaskService::model5, ml5Dto);
+    }
 
-        String url = FLASK_API_URL + "5";
+    @Override
+    public String predictM6(ML6Dto ml6Dto) {
+        validation.model6Validation(ml6Dto);
+        return getPrediction(flaskService::model6, ml6Dto);
+    }
 
-        try {
-            ResponseEntity<String> response = restTemplate.postForEntity(url, ml5Dto, String.class);
-
-            return response.getBody();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "Error: Unable to get response from Flask!";
-        }
+    @Override
+    public String predictM7(ML7Dto ml7Dto) {
+        validation.model7Validation(ml7Dto);
+        return getPrediction(flaskService::model7, ml7Dto);
     }
 }
