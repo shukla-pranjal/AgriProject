ec334f6f465bcc80dae0e583925cc32451767c3e
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-03-30 11:21:21 +0530

    feat(entity): define Cart entity for database mapping of user carts

--- AgriProject_java/src/main/java/com/agriproject/enitity/Cart.java
+++ AgriProject_java/src/main/java/com/agriproject/enitity/Cart.java
@@ -7,15 +7,20 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
-@ToString
-@EntityListeners(AuditingEntityListener.class)
-public class Cart extends BaseModel{
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cart extends BaseModel {
+
     @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
-    private Long id;  
-    private String name;
-    private String description;  
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> items;
+
+    private boolean active = true;
 }
