5a8fdec91a6c6c6fb18c3c659bd4d33a790072e7
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-04-05 11:23:21 +0530

    ⭐ Added ReviewsDTO to handle user-generated product feedback

---
+++ AgriProject_java/src/main/java/com/agriproject/dto/ReviewsDTO.java
@@ -0,0 +1,18 @@
+package com.agriproject.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReviewsDTO {
+    private Long id;
+    private ProductDTO product;
+    private UserDTO user;
+    private int rating;
+    private String comment;
+    private Date reviewDate;
+}
