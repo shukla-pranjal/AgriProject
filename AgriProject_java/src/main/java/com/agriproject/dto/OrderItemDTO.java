be1c200dcfbad019bce709555a6689221b9a7bc1
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-04-08 11:23:21 +0530

    📦 Introduced OrderItemDTO for item-level detail in orders

---
+++ AgriProject_java/src/main/java/com/agriproject/dto/OrderItemDTO.java
@@ -0,0 +1,15 @@
+package com.agriproject.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItemDTO {
+    private Long id;
+    private ProductDTO product;
+    private int quantity;
+    private double price;
+}
