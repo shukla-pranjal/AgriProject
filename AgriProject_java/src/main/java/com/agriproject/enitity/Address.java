8371ef6619fac0f9567f984973948835531c7dc3
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-04-10 11:23:21 +0530

    🏠 Created Address entity for user and order location binding

---
+++ AgriProject_java/src/main/java/com/agriproject/enitity/Address.java
@@ -0,0 +1,34 @@
+package com.agriproject.enitity;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@ToString
+@EntityListeners(AuditingEntityListener.class)
+public class Address extends BaseModel {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String street;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String country;
+}
