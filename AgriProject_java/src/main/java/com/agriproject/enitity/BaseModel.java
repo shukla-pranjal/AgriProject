9fc00745a02230f71f8636cea4dc07bef40a62f4
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-04-11 11:23:21 +0530

    🧱 Introduced BaseModel as foundation for common entity fields

---
+++ AgriProject_java/src/main/java/com/agriproject/enitity/BaseModel.java
@@ -0,0 +1,37 @@
+package com.agriproject.enitity;
+
+import java.util.Date;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@MappedSuperclass
+@ToString
+public abstract class BaseModel {
+
+    @CreatedBy
+    @Column(updatable = false)
+    private Integer createdBy;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private Date createdOn;
+
+    @LastModifiedBy
+    @Column(insertable  = false)
+    private Integer updateBy;
+
+    @LastModifiedDate
+    @Column(insertable  = false)
+    private Date updateOn;
+}
