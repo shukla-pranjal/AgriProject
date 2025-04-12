59ef72c86cbb12e1e49d2602f84f4f8d78bb1801
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-04-12 11:23:21 +0530

    📂 Added Category entity to group and classify products

---
+++ AgriProject_java/src/main/java/com/agriproject/enitity/Category.java
@@ -0,0 +1,31 @@
+package com.agriproject.enitity;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@ToString
+@EntityListeners(AuditingEntityListener.class)
+public class Category extends BaseModel{
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;  
+    private String name;
+    private String description;  
+}
