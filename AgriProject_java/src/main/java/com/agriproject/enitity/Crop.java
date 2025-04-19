41f5f6c9f7e0ad25b2432c926ffd15a905149c62
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-04-19 11:23:22 +0530

    🌾 Integrated Crop entity for storing crop-related data

---
+++ AgriProject_java/src/main/java/com/agriproject/enitity/Crop.java
@@ -0,0 +1,35 @@
+package com.agriproject.enitity;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@ToString
+@EntityListeners(AuditingEntityListener.class)
+public class Crop {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String cropType; 
+    
+    private String season; 
+    
+}
