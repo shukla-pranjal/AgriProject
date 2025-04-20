1635ff287c58b5e2d00a12bf9e95e4356de7a099
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-04-20 11:28:30 +0530

    Improved CropService functionality to handle new crop categories and services

--- AgriProject_java/src/main/java/com/agriproject/service/CropService.java
+++ AgriProject_java/src/main/java/com/agriproject/service/CropService.java
@@ -1,4 +1,17 @@
 package com.agriproject.service;
 
+import com.agriproject.dto.CropDTO;
+
+import java.util.List;
+
 public interface CropService {
+    List<CropDTO> getAllCrops();
+
+    CropDTO getCropById(Long id) throws Exception;
+
+    CropDTO createCrop(CropDTO cropDTO);
+
+    CropDTO updateCrop(Long id, CropDTO cropDTO) throws Exception;
+
+    void deleteCrop(Long id) throws Exception;
 }
