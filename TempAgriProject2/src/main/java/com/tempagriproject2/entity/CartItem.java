d560d8c7ccd15acd9f9a5b7425b4e0edbbec9e85
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-05-09 19:26:09 +0530

    feat(entity): add CartItem entity linking cart to products and quantities

--- TempAgriProject2/src/main/java/com/tempagriproject2/entity/CartItem.java
+++ TempAgriProject2/src/main/java/com/tempagriproject2/entity/CartItem.java
@@ -1,6 +1,8 @@
 package com.tempagriproject2.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
 import lombok.*;
 
 @Getter
@@ -10,6 +12,9 @@ import lombok.*;
 @Entity
 @ToString
 public class CartItem extends BaseModel {
-    private String name;
-    private String description;
+    @ManyToOne
+    private Cart cart;
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Product product;
+    private Integer quantity;
 }
