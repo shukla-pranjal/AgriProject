952a253c9b17dc4f107ba3c3ccbe7f497d938a5f
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-05-13 19:26:09 +0530

    feat(repository): introduce ProductRepository for database access

--- TempAgriProject2/src/main/java/com/tempagriproject2/repository/ProductRepository.java
+++ TempAgriProject2/src/main/java/com/tempagriproject2/repository/ProductRepository.java
@@ -1,4 +1,14 @@
 package com.tempagriproject2.repository;
 
-public class ProductRepository {
+import com.tempagriproject2.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository <Product, Integer> {
+    List<Product> findByCategoryId(Integer categoryId);
+
+    List<Product> findByFarmerId(Integer farmerId);
 }
