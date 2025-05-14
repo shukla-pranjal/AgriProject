4577cdfdb6cecf19475eb7eee5cb14005f483c8e
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-05-14 19:26:09 +0530

    feat(repository): create ReviewRepository to support review queries

--- TempAgriProject2/src/main/java/com/tempagriproject2/repository/ReviewRepository.java
+++ TempAgriProject2/src/main/java/com/tempagriproject2/repository/ReviewRepository.java
@@ -1,4 +1,15 @@
 package com.tempagriproject2.repository;
 
-public class ReviewRepository {
+import com.tempagriproject2.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+    List<Review> findByProductId(Integer productId);
+
+    List<Review> findByUserId(Integer userId);
+
+    Optional<Review> findByProductIdAndUserId(Integer productId, Integer userId);
 }
