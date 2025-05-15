e7039c36166d3e689e66a8e3febc9ce77b245740
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-05-15 19:26:09 +0530

    feat(service): define ProductService interface for product-related operations

--- TempAgriProject2/src/main/java/com/tempagriproject2/service/ProductService.java
+++ TempAgriProject2/src/main/java/com/tempagriproject2/service/ProductService.java
@@ -1,4 +1,23 @@
 package com.tempagriproject2.service;
 
-public class ProductService {
+import com.tempagriproject2.dto.ProductDTO;
+
+import java.util.List;
+
+public interface ProductService {
+
+    List<ProductDTO> getAllProducts();
+
+    ProductDTO getProductById(Integer id) throws Exception;
+
+    ProductDTO createProduct(ProductDTO productDTO) throws Exception;
+
+    ProductDTO updateProduct(Integer id, ProductDTO productDTO) throws Exception;
+
+    void deleteProduct(Integer id) throws Exception;
+
+    List<ProductDTO> getProductsByCategoryId(Integer categoryId) throws Exception;
+
+    List<ProductDTO> getProductsByFarmerId(Integer farmerId) throws Exception;
 }
+
