8dcbb36b1965fe75a691f14b9df4af5bc639d248
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-05-15 19:26:09 +0530

    feat(service): create ReviewService interface to handle review logic

--- TempAgriProject2/src/main/java/com/tempagriproject2/service/ReviewService.java
+++ TempAgriProject2/src/main/java/com/tempagriproject2/service/ReviewService.java
@@ -1,4 +1,22 @@
 package com.tempagriproject2.service;
 
+import com.tempagriproject2.dto.ReviewDTO;
+
+import java.util.List;
+
 public interface ReviewService {
+
+    List<ReviewDTO> getAllReviews();
+
+    ReviewDTO getReviewById(Integer id) throws Exception;
+
+    ReviewDTO createReview(ReviewDTO reviewDTO) throws Exception;
+
+    ReviewDTO updateReview(ReviewDTO reviewDTO) throws Exception;
+
+    void deleteReview(Integer id) throws Exception;
+
+    List<ReviewDTO> getReviewsByProductId(Integer productId)throws Exception;
+
+    List<ReviewDTO> getReviewsByUserId(Integer userId) throws Exception;
 }
