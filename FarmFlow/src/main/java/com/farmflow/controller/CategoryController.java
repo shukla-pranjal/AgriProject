1fcbacf04c3afbaaf221a7ca71fff7e7fe1955d8
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-06-14 22:29:50 +0530

    Optimized cart and category controllers for enhanced shopping experience and category management.

--- FarmFlow/src/main/java/com/farmflow/controller/CategoryController.java
+++ FarmFlow/src/main/java/com/farmflow/controller/CategoryController.java
@@ -17,102 +17,56 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@Slf4j
 public class CategoryController implements CategoryEndpoint {
 
     private final CategoryService categoryService;
-    private static final String CLASS_NAME = CategoryController.class.getSimpleName();
 
     @Override
     public ResponseEntity<?> getAll() {
-        String methodName = "getAll";
-        log.debug("{} : {} :: Started", CLASS_NAME, methodName);
         List<CategoryDTO> categories = categoryService.getAllCategories();
-        log.info("{} : {} :: Successfully retrieved {} categories", CLASS_NAME, methodName, categories.size());
         return CommonUtil.createBuildResponse(categories, HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<?> getById(Integer id) throws Exception {
-        String methodName = "getById";
-        log.debug("{} : {} :: Started with id: {}", CLASS_NAME, methodName, id);
-        try {
-            CategoryDTO category = categoryService.getCategoryById(id);
-            log.info("{} : {} :: Successfully retrieved category for id: {}", CLASS_NAME, methodName, id);
-            return CommonUtil.createBuildResponse(category, HttpStatus.OK);
-        } catch (Exception e) {
-            log.error("{} : {} :: Failed to retrieve category for id: {}, error: {}", CLASS_NAME, methodName, id, e.getMessage());
-            throw e;
-        }
+        CategoryDTO category = categoryService.getCategoryById(id);
+        return CommonUtil.createBuildResponse(category, HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<?> create(CategoryDTO categoryDTO) throws Exception {
-        String methodName = "create";
-        log.debug("{} : {} :: Started with categoryDTO: {}", CLASS_NAME, methodName, categoryDTO);
-        try {
-            CategoryDTO created = categoryService.createCategory(categoryDTO);
-            log.info("{} : {} :: Successfully created category", CLASS_NAME, methodName);
-            return CommonUtil.createBuildResponse(created, HttpStatus.CREATED);
-        } catch (Exception e) {
-            log.error("{} : {} :: Failed to create category, error: {}", CLASS_NAME, methodName, e.getMessage());
-            throw e;
-        }
+        CategoryDTO created = categoryService.createCategory(categoryDTO);
+        return CommonUtil.createBuildResponse(created, HttpStatus.CREATED);
     }
 
     @Override
     public ResponseEntity<?> update(Integer id, CategoryDTO categoryDTO) throws Exception {
-        String methodName = "update";
-        log.debug("{} : {} :: Started with id: {}, categoryDTO: {}", CLASS_NAME, methodName, id, categoryDTO);
-        try {
-            CategoryDTO updated = categoryService.updateCategory(id, categoryDTO);
-            log.info("{} : {} :: Successfully updated category for id: {}", CLASS_NAME, methodName, id);
-            return CommonUtil.createBuildResponse(updated, HttpStatus.OK);
-        } catch (Exception e) {
-            log.error("{} : {} :: Failed to update category for id: {}, error: {}", CLASS_NAME, methodName, id, e.getMessage());
-            throw e;
-        }
+        CategoryDTO updated = categoryService.updateCategory(id, categoryDTO);
+        return CommonUtil.createBuildResponse(updated, HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<?> delete(Integer id) throws Exception {
-        String methodName = "delete";
-        log.debug("{} : {} :: Started with id: {}", CLASS_NAME, methodName, id);
-        try {
-            categoryService.deleteCategory(id);
-            log.info("{} : {} :: Successfully deleted category for id: {}", CLASS_NAME, methodName, id);
-            return CommonUtil.createBuildResponseMessage("Category deleted successfully", HttpStatus.OK);
-        } catch (Exception e) {
-            log.error("{} : {} :: Failed to delete category for id: {}, error: {}", CLASS_NAME, methodName, id, e.getMessage());
-            throw e;
-        }
+        categoryService.deleteCategory(id);
+        return CommonUtil.createBuildResponseMessage("Category deleted successfully", HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<?> searchCategories(String name, String description) {
-        String methodName = "searchCategories";
-        log.debug("{} : {} :: Started with name: {}, description: {}", CLASS_NAME, methodName, name, description);
         List<CategoryDTO> categories = categoryService.searchCategories(name, description);
-        log.info("{} : {} :: Successfully retrieved {} categories", CLASS_NAME, methodName, categories.size());
         return CommonUtil.createBuildResponse(categories, HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<?> getAllCategoriesPaged(PaginationRequest paginationRequest) {
-        String methodName = "getAllCategoriesPaged";
-        log.debug("{} : {} :: Started with paginationRequest: {}", CLASS_NAME, methodName, paginationRequest);
         Page<CategoryDTO> page = categoryService.getAllCategoriesPaged(paginationRequest);
-        log.info("{} : {} :: Successfully retrieved paged categories, page: {}, size: {}", CLASS_NAME, methodName, page.getNumber(), page.getSize());
         PaginatedResponse<CategoryDTO> paginatedResponse = PaginatedResponse.fromPage(page);
         return CommonUtil.createBuildResponse(paginatedResponse, HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<?> searchCategoriesPaged(PaginationRequest paginationRequest, String name, String description) {
-        String methodName = "searchCategoriesPaged";
-        log.debug("{} : {} :: Started with paginationRequest: {}, name: {}, description: {}", CLASS_NAME, methodName, paginationRequest, name, description);
         Page<CategoryDTO> page = categoryService.searchCategoriesPaged(paginationRequest, name, description);
-        log.info("{} : {} :: Successfully retrieved paged categories, page: {}, size: {}", CLASS_NAME, methodName, page.getNumber(), page.getSize());
         PaginatedResponse<CategoryDTO> paginatedResponse = PaginatedResponse.fromPage(page);
         return CommonUtil.createBuildResponse(paginatedResponse, HttpStatus.OK);
     }
